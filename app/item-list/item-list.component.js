c7a3bf7c305492289e7c947c5a0c30398e1dcbb9
Johannes Wirth <jojowi@gmx.net>
2016-11-07 15:43:48 +0100

    Initial commit

---
+++ app/item-list/item-list.component.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Register `phoneList` component, along with its associated controller and template
+angular.
+  module('itemList').
+  component('itemList', {
+    templateUrl: 'item-list/item-list.template.html',
+    controller: ['Item',
+      function ItemListController(Item) {
+        var self = this;
+        this.items = Item.query({address:"items"});
+        this.orderProp = "id";
+        this.sum = function sum(items){
+            var res = 0;
+            var item;
+            for (item of items){
+                res += item.count * item.price;
+            }
+            return res;
+        }
+        this.add = function add(){
+            var item = {id:this.itemID,name:this.itemName,count:0,price:this.itemPrice};
+            Item.saveItem({address:"additem"},item);
+            this.items.push(item);
+            this.itemID = "";
+            this.itemName = "";
+            this.itemPrice = "";
+        }
+        this.remove = function remove(item){
+            Item.removeItem({address:"removeitem"},item);
+            this.items.splice(this.items.indexOf(item),1);
+        }
+      }
+    ]
+  });
